f32cfea5fe7d912ff666174eb1fdf6ff1f69824f
Atul Mundaware <mundawareatul1@gmail.com>
2018-10-06 22:31:11 +0530

    -- initial Commit

---
+++ src/main/java/com/atul/ethereum/SpringBootEthereumOpsApplication.java
@@ -0,0 +1,12 @@
+package com.atul.ethereum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEthereumOpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEthereumOpsApplication.class, args);
+	}
+}
